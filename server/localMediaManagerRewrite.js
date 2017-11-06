7b861c65109d9b2b500b6de7e1f58b4ec4bad8e9
morpheous <cerbus.collin@gmail.com>
2017-11-06 00:25:13 -0800

    43

--- server/localMediaManagerRewrite.js
+++ server/localMediaManagerRewrite.js
@@ -53,11 +53,12 @@ const h1 = "HARD_DRIVE.";
 	//if ( ek.length > 0 ) { await RU.delKeys( redis , ek ); }
 	//console.log( "done cleansing instance" );
 	
+	//var mp = await require( "./utils/localMedia_Util" ).findAndMountUSB_From_UUID( "2864E38A64E358D8" );
+	var mp = "/home/morpheous/TMP2/EMULATED_MOUNT_PATH";
+	await RU.setKey( redis , "HARD_DRIVE.MOUNT_POINT" , mp );
+	GLOBAL_INSTANCE_MOUNT_POINT = mp;
+
 	if ( ek.length < 1 ) { 
-		//var mp = await require( "./utils/localMedia_Util" ).findAndMountUSB_From_UUID( "2864E38A64E358D8" );
-		var mp = "/home/morpheous/TMP2/EMULATED_MOUNT_PATH";
-		await RU.setKey( redis , "HARD_DRIVE.MOUNT_POINT" , mp );
-		GLOBAL_INSTANCE_MOUNT_POINT = mp;
 		var x1 = await require( "./utils/localMedia_Util" ).buildHardDriveReference( mp ); // we alll know this is cancer. but fml
 		for ( var wGenre in x1 ) {
 			var x1Shows = Object.keys( x1[ wGenre ] );
@@ -84,7 +85,7 @@ const h1 = "HARD_DRIVE.";
 	await RU.setMulti( redis , [
 		[ "set" , "LAST_SS.ACTIVE_STATE" , "LOCAL_MEDIA" ] ,
 		[ "set" , R_LM_Config_Genre , "TVShows" ] ,
-		[ "set" , R_LM_Config_AdvanceShow , "false" ] ,
+		[ "set" , R_LM_Config_AdvanceShow , "true" ] ,
 		[ "set" , R_LM_Config_SpecificShow , "false" ] ,
 		[ "set" , R_LM_Config_SpecificEpisode , "false" ] ,
 	]);
@@ -116,12 +117,11 @@ function calculateNext( lastPlayed , config ) {
 			const R_N_BASE = "HARD_DRIVE." + lastPlayed.genre + ".";
 
 			var F_UNEQ_IDX = lastPlayed.uneq_idx;
-			var F_Episode_IDX = 0;
-			var F_Season_IDX = lastPlayed.season_idx;
 			var F_ShowName = lastPlayed.show_name;
+			var F_Episode_IDX = lastPlayed.episode_idx;
+			var F_Season_IDX = lastPlayed.season_idx;
 			var F_FP = "";
-			var F_RemainingTime , F_CurrentTime , F_ThreePercent , F_Duration = 0;
-			var nextEpisode = 0;
+			var F_RemainingTime , F_CurrentTime , F_ThreePercent , F_Duration , nextEpisode = 0;
 
 			if ( config[ 3 ] === "true" ) { // IF Specific-Episode
 				
@@ -130,32 +130,65 @@ function calculateNext( lastPlayed , config ) {
 
 			}
 			else if ( config[ 1 ] === "true" ) { // IF Advance-Next-Show is Enabled
-				
+				console.log( "inside Advance-Next-Show" );
+				F_UNEQ_IDX = ( F_UNEQ_IDX + 1 );
+				const R_NextShow = R_N_BASE + "META.UNEQ";
+				F_ShowName = await RU.getFromSetByIndex( redis , R_NextShow , F_UNEQ_IDX );
+				if ( F_ShowName === null ) { //  IF Advanced Past Total-UNEQ-aka-Unique Shows in Genre
+					console.log( "inside show-in-genre reset" );
+					F_UNEQ_IDX = 0;
+					F_ShowName = await RU.getFromSetByIndex( redis , R_NextShow , F_UNEQ_IDX );
+				}
+
+				// Check if Show Already Has a Previous Position
+				// This is the only point of double saving the **nowPlaying** obj into G_R_NP_ShowName_Backup
+				const R_PreviouslyWatched = "LAST_SS.LOCAL_MEDIA." + lastPlayed.genre + "." + F_ShowName;
+				var previouslyWatched = await RU.getKey( redis , R_PreviouslyWatched );
+				if ( previouslyWatched !== null ) {
+					lastPlayed = JSON.parse( previouslyWatched );
+					lastPlayed.completed = true;
+					F_Episode_IDX = lastPlayed.episode_idx;
+					F_Season_IDX = lastPlayed.season_idx
+				}
+				else {
+					F_Episode_IDX = -1, 
+					F_Season_IDX = 0;
+				}
+				await callNextGen1Build();
+
 			}
 			else { // Just continue then to +1-episode based on lastPlayed
-				
-				nextEpisode = ( lastPlayed.episode_idx + 1 );
-				F_Episode_IDX = nextEpisode;
-				const R_Next_EP = R_N_BASE + "FP." + lastPlayed.show_name + "." + lastPlayed.season_idx;
-				nextEpisode = await RU.getFromSetByIndex( redis , R_Next_EP , nextEpisode );
-				console.log( "next episode === " + nextEpisode );
+				await callNextGen1Build(); 
+			}
 
-				if ( nextEpisode === null ) { // IF Advanced Past Total-Episodes in Season Boundry
-					console.log( "inside episode reset" );
-					F_Episode_IDX = 0;
-					F_Season_IDX = ( lastPlayed.season_idx + 1 );
-					var R_Next_Season = R_N_BASE + "FP." + lastPlayed.show_name + "." + F_Season_IDX;
-					var intermediaryNext_Episode = await RU.getFromSetByIndex( redis , R_Next_Season , 0 );
-					if ( intermediaryNext_Episode === null ) { // IF Advanced Past Total-Seasons in Show Boundry
-						console.log( "inside season reset" );
-						F_Season_IDX = 0;
-						R_Next_Season = R_N_BASE + "FP." + lastPlayed.show_name + "." + F_Season_IDX;
-						F_FP = await RU.getFromSetByIndex( redis , R_Next_Season , 0 );
+			function callNextGen1Build() {
+				return new Promise( async function( resolve , reject ) {
+					try {
+						nextEpisode = ( F_Episode_IDX + 1 );
+						F_Episode_IDX = nextEpisode;
+						const R_Next_EP = R_N_BASE + "FP." + F_ShowName + "." + F_Season_IDX;
+						nextEpisode = await RU.getFromSetByIndex( redis , R_Next_EP , nextEpisode );
+						console.log( "next episode === " + nextEpisode );
+
+						if ( nextEpisode === null ) { // IF Advanced Past Total-Episodes in Season Boundry
+							console.log( "inside episode reset" );
+							F_Episode_IDX = 0;
+							F_Season_IDX = ( F_Season_IDX + 1 );
+							var R_Next_Season = R_N_BASE + "FP." + F_ShowName + "." + F_Season_IDX;
+							var intermediaryNext_Episode = await RU.getFromSetByIndex( redis , R_Next_Season , 0 );
+							if ( intermediaryNext_Episode === null ) { // IF Advanced Past Total-Seasons in Show Boundry
+								console.log( "inside season reset" );
+								F_Season_IDX = 0;
+								R_Next_Season = R_N_BASE + "FP." + F_ShowName + "." + F_Season_IDX;
+								F_FP = await RU.getFromSetByIndex( redis , R_Next_Season , 0 );
+							}
+							else { F_FP = intermediaryNext_Episode; }
+						}
+						else { F_FP = nextEpisode; }
+						resolve();
 					}
-					else { F_FP = intermediaryNext_Episode; }
-				}
-				else { F_FP = nextEpisode; }
-
+					catch( error ) { console.log( error ); reject( error ); }
+				});
 			}
 
 			// Adjust Final-Full-File-Path from Redis "set" language
@@ -163,8 +196,8 @@ function calculateNext( lastPlayed , config ) {
 			var xb2 = ( F_Season_IDX + 1 ).toString();
 			if ( F_Season_IDX < 10 ) { F_FP = xb1 + "/0" + xb2 + "/" + F_FP; }
 			else { F_FP = xb1 + "/" + xb2 + "/" + F_FP; }
-			
-			resolve({
+
+			var r1 = {
 				genre: lastPlayed.genre ,
 				uneq_idx: F_UNEQ_IDX ,
 				show_name: F_ShowName ,
@@ -176,7 +209,11 @@ function calculateNext( lastPlayed , config ) {
 				three_percent: F_ThreePercent ,
 				duration: F_Duration ,
 				cur_time: F_CurrentTime
-			});
+			};
+			console.log( "\n" );
+			console.log( r1 );
+			
+			resolve( r1 );
 
 		}
 		catch( error ) { console.log( error ); reject( error ); }
@@ -196,7 +233,8 @@ async function wPlay() {
 	
 	var liveConfig = await RU.getMultiKeys( redis , R_LM_Config_Genre , R_LM_Config_AdvanceShow , R_LM_Config_SpecificShow , R_LM_Config_SpecificEpisode );
 	
-	const R_Live_Genre_NP = R_LocalMedia_Base + liveConfig[ 0 ] + ".NOW_PLAYING";
+	const R_Live_Genre_Base = R_LocalMedia_Base + liveConfig[ 0 ];
+	const R_Live_Genre_NP = R_Live_Genre_Base + ".NOW_PLAYING";
 	var liveLastPlayed = await RU.getKey( redis , R_Live_Genre_NP )
 	if ( liveLastPlayed === null ) { // Nothing Ever Played In This Genre
 		console.log( "genre is FRESH !!" );
@@ -209,7 +247,7 @@ async function wPlay() {
 			genre: liveConfig[ 0 ] , uneq_idx: 0 , show_name: showName , 
 			season_idx: 0 , episode_idx: 0 , fp: firstEpFullPath , 
 			completed: false , cur_time: 0 , remaining_time: 0 , 
-			three_percent: 0 ,duration: 0
+			three_percent: 0 , duration: 0
 		};
 	}
 	else { // NOT Genre Fresh
@@ -220,15 +258,18 @@ async function wPlay() {
 		else { FinalNowPlaying = await calculateNext( liveLastPlayed , liveConfig ); } // ELSE , calculate the **Next** playing-obj
 	}
 
-	if ( FinalNowPlaying.three_percent == 0 ) {
+	if ( !FinalNowPlaying.three_percent ) { FinalNowPlaying.three_percent = 0; }
+	if ( FinalNowPlaying.three_percent === 0 ) {
 		FinalNowPlaying.duration = wGetDuration( FinalNowPlaying.fp );
 		FinalNowPlaying.three_percent = Math.floor( ( FinalNowPlaying.duration - ( FinalNowPlaying.duration * 0.025 ) ) );
+		console.log( "Duration === " + FinalNowPlaying.duration.toString() );
+		console.log( "Three Percent === " + FinalNowPlaying.three_percent.toString() );
 	}
 
 	console.log( FinalNowPlaying );
 
 	const x1 = JSON.stringify( FinalNowPlaying );
-	const R_NP_ShowName_BackupKey = R_Live_Genre_NP + "." + FinalNowPlaying.show_name;
+	const R_NP_ShowName_BackupKey = R_Live_Genre_Base + "." + FinalNowPlaying.show_name;
 	await RU.setMulti( redis , [ [ "set" , R_Live_Genre_NP , x1 ] ,  [ "set" , R_NP_ShowName_BackupKey , x1 ] ]);
 
 
@@ -249,9 +290,14 @@ async function wPlay() {
 function updateLastPlayedTime( wTime ) {
 	return new Promise( async function( resolve , reject ) {
 		try {
-			G_NOW_PLAYING.cur_time = wTime;
-			G_NOW_PLAYING.remaining_time = ( G_NOW_PLAYING.duration - G_NOW_PLAYING.cur_time );
-			if ( G_NOW_PLAYING.cur_time >= G_NOW_PLAYING.three_percent ) { G_NOW_PLAYING.completed = true; }
+			if ( wTime ) { console.log( "wTime === " + wTime.toString() ); }
+			else { console.log( "no wTIME !!!!" ); }
+			if ( wTime !== null ) {
+				G_NOW_PLAYING.cur_time = wTime;
+				G_NOW_PLAYING.remaining_time = ( G_NOW_PLAYING.duration - G_NOW_PLAYING.cur_time );
+				if ( G_NOW_PLAYING.cur_time >= G_NOW_PLAYING.three_percent ) { G_NOW_PLAYING.completed = true; }
+			}
+			else { G_NOW_PLAYING.completed = true; }
 			var x1 = JSON.stringify( G_NOW_PLAYING );
 			await RU.setMulti( redis , [ [ "set" , G_R_Live_Genre_NP , x1 ] ,  [ "set" , G_R_NP_ShowName_Backup , x1 ] ]);			
 			resolve();
