d9a7e844e0ffdbe99d3b1115012c4f631ecae6f4
morpheous <cerbus.collin@gmail.com>
2017-11-18 23:25:29 -0800

    66

--- server/STATES/Skype_Foreground.js
+++ server/STATES/Skype_Foreground.js
@@ -1,3 +1,26 @@
+// Apparently as of Today , 18NOV2017 , the old skype version I am using
+// completely no longer works. 
+// Somehow this --> https://github.com/tomorgan/SkypeWebSDKSamples/blob/gh-pages/AV/5-MakingVideoCalls/index.htm
+// http://code.skypewebsdk.com/
+// https://msdn.microsoft.com/en-gb/skype/websdk/docs/objectmodel
+// https://msdn.microsoft.com/en-gb/skype/websdk/docs/gettingstarted#sectionSection2
+// https://msdn.microsoft.com/Skype/WebSDK/docs/GettingStarted
+// https://msdn.microsoft.com/en-gb/skype/websdk/docs/skypewebsdk
+// https://msdn.microsoft.com/en-gb/skype/websdk/docs/getapientrysignin
+// https://github.com/OfficeDev/skype-docs
+// https://www.thoughtstuff.co.uk/skype-web-sdk
+// https://github.com/OfficeDev/skype-web-sdk-samples
+// or this --> https://vsee.com/
+// or this --> https://tokbox.com/
+// or this --> http://peerjs.com/
+// or this --> https://gist.github.com/kalkov/1744211
+// or this --> https://media.twiliocdn.com/sdk/js/video/releases/1.6.0/docs/
+// https://www.twilio.com/docs/api/video/javascript-v1-getting-started
+// or this --> https://trueconf.com/downloads/linux.html
+
+// or this --> https://webrtchacks.com/whats-in-a-webrtc-javascript-library/
+// https://docs.google.com/document/d/1idl_NYQhllFEFqkGQOLv8KBK8M3EVzyvxnKkHl4SuM8/edit#
+
 const wSkypeNames = require("../../personal.js").skypeNames;
 function wStart( wOptions ) {
 	return new Promise( async function( resolve , reject ) {
